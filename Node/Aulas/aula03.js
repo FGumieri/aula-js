79943a797d14b1b9f8b0220b244959458810c2a2
FGumieri <fernandogumieri@gmail.com>
2020-09-22 20:47:28 -0300

    :package: Add arquivo node no repositório

---
+++ Node/Aulas/aula03.js
@@ -0,0 +1,9 @@
+let idade = 31
+let nome = 'Fernando'
+
+if (idade <= 17) {
+    console.log(`${nome}, Você NÃO pode dirigir!`)
+}
+else if (idade >= 18) {
+    console.log(`${nome}, Você pode dirigir!`)
+}
