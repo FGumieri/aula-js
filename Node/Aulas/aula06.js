851c35aee16d9019a31c3d9d06cdc48d0da9e54e
migueldossantos011@gmail.com <migueldossantos011@gmail.com>
2020-10-01 19:51:55 -0300

    :package: bug_correcao de codigo passagem de parametros :man_technologist:

--- Node/Aulas/aula06.js
+++ Node/Aulas/aula06.js
@@ -1,6 +1,5 @@
 function Somar() {
-    let num1 = 10
-    let num2 = 5
-    let result = num1 + num2
-    console.log(result)
+    // passar parametros para funcao e printar a soma desses parametros
+    // let result = num1 + num2
+    // console.log(result)
 }
