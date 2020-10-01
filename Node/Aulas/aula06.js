e1cd1d48ac50f54986ab49df0a1e381266b839c5
FGumieri <fernandogumieri@gmail.com>
2020-10-01 20:48:26 -0300

    :package: array com for dentro da funcao part2

--- Node/Aulas/aula06.js
+++ Node/Aulas/aula06.js
@@ -1,16 +1,21 @@
+// criar um array com 12 numeros representando resultado mensal
+// mapear o array usando for
+// somar todos os numeros desse array
 function Somar() {
     let salarios = [600, 600, 600, 600, 600, 600, 600, 600, 600, 600, 600, 600]
     let resultadoSalarios = 0
 
     for (let i = 0; i < salarios.length; i++) {
         resultadoSalarios += salarios[i]
-        console.log(resultadoSalarios)
     }  
-    // criar um array com 12 numeros representando resultado mensal
-    // mapear o array usando for
-    // somar todos os numeros desse array
-    // criar a funcao subtrair com base na somar
-
+    console.log(resultadoSalarios)
 }
+
+
+// criar a funcao subtrair com base na somar
+function Subtrair() {
+    
+}
+
 
 Somar()
