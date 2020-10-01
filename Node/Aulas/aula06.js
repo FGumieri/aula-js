9c062ec71bbe650e5fd2056a2a439dd1b7ee14d7
FGumieri <fernandogumieri@gmail.com>
2020-10-01 20:04:03 -0300

    :package: aprendendo propriedades na função

--- Node/Aulas/aula06.js
+++ Node/Aulas/aula06.js
@@ -1,5 +1,8 @@
-function Somar() {
+function Somar(num1, num2) {
     // passar parametros para funcao e printar a soma desses parametros
     // let result = num1 + num2
-    // console.log(result)
+    console.log(num1 + num2)
 }
+
+Somar(5, 5)
+Somar(10, 10)
